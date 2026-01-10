94b0f7e2c5006cfcfe5b6234331d6c354a921cdf
ShadaMaitexa <shada.maitexa@gmail.com>
2026-01-10 15:57:37 +0530

    appbar changed

--- lib/signup.dart
+++ lib/signup.dart
@@ -60,15 +60,6 @@ class _SignUpState extends State<SignUp> {
           icon: Icon(Icons.arrow_back, color: Color(0xFF2C3E50)),
           onPressed: () => Navigator.pop(context),
         ),
-        title: Text(
-          'Sign Up',
-          style: TextStyle(
-            color: Color(0xFF2C3E50),
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
       ),
       body: SafeArea(
         child: Center(
